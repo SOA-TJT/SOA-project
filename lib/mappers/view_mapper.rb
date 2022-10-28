b09f80c65a71e7be5ddee1d23a40c8a62d8fb07d
Hsuan <lala092325@gmail.com>
2022-10-28 09:29:57 +0800

    remove creator

--- lib/mappers/view_mapper.rb
+++ lib/mappers/view_mapper.rb
@@ -11,7 +11,7 @@ module LightofDay
     class ViewMapper
       def initialize(un_token, topicid, gateway_class = Unsplash::Api)
         @token = un_token
-        @gateway = gateway_class.new("https://api.unsplash.com/photos/random/?topics=#{topicid}&orientation=landscape")
+        @gateway = gateway_class.new("https://api.unsplash.com/photos/random/?topics=#{topicid}&orientation=landscape", @token)
       end
 
       def find_a_photo
@@ -34,12 +34,10 @@ module LightofDay
 
         def build_entity
           LightofDay::Unsplash::Entity::View.new(
-            topic_id:,
-            title:,
-            description:,
-            topic_url:
-          )
-          LightofDay::Unsplash::Entity::Creator.new(
+            topic:,
+            width:,
+            height:,
+            urls:,
             name:,
             bio:,
             image:
@@ -67,7 +65,7 @@ module LightofDay
         end
 
         def bio
-          @data['user']['bio']
+          @data['user']['bio'] ? @data['user']['bio'] : ''
         end
 
         def image
